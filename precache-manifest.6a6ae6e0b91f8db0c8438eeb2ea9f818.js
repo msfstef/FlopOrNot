d4c1961159b30c5642bc7c80cfea0ca0208f0b25
Stefanos Mousafeiris <sm6114@imperial.ac.uk>
2019-03-17 21:07:33 +0200

    Updates

---
+++ precache-manifest.6a6ae6e0b91f8db0c8438eeb2ea9f818.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "27ea269eee628839d50c",
+    "url": "/FlopOrNot/static/js/runtime~main.b718d01a.js"
+  },
+  {
+    "revision": "1182fb2900c95fe21dcc",
+    "url": "/FlopOrNot/static/js/main.818511ed.chunk.js"
+  },
+  {
+    "revision": "e6cf5f1ce1ca7814b2e8",
+    "url": "/FlopOrNot/static/js/2.4df34fc1.chunk.js"
+  },
+  {
+    "revision": "1182fb2900c95fe21dcc",
+    "url": "/FlopOrNot/static/css/main.98238814.chunk.css"
+  },
+  {
+    "revision": "c57c570db233f6c69a6e05bec1c55778",
+    "url": "/FlopOrNot/index.html"
+  }
+];
